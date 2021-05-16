edd53bc4eef7732a86532c491f215aa917ad4074
Asus PS-W10 <zagerer@gmail.com>
2021-05-16 17:14:54 -0500

    Tutorial server completed

--- tutorial/server.py
+++ tutorial/server.py
@@ -6,3 +6,10 @@ host = socket.gethostname()
 port = 9999
 server_socket.bind( (host, port) )
 
+server_socket.listen(5)
+while True:
+    client_socket, addr = server_socket.accept()
+    print("Connected with", addr, "on port", port)
+    current_time = time.ctime(time.time()) + "\r\n"
+    client_socket.send( current_time.encode('ascii') )
+    client_socket.close()
